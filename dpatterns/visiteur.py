38f584397b09031f15a9a1b73e0685f909c4ee4d
Darko Stankovski <darko.itpro@gmaiL.com>
2025-06-05 03:04:12 +0200

    Mise à jour visiteur.

--- dpatterns/visiteur.py
+++ dpatterns/visiteur.py
@@ -1,10 +1,21 @@
-class DisplayVisiter:
+from abc import ABC, abstractmethod
+
+class GeometricVisiter(ABC):
+    @abstractmethod
+    def visite_carre(self, carre):
+        pass
+
+    @abstractmethod
+    def visite_cercle(self, cercle):
+        pass
+
+class DisplayVisiter(GeometricVisiter):
     def visite_carre(self, carre):
         print("visite du carré")
     def visite_cercle(self, cercle):
         print("visite cercle")
 
-class CalculationVisiter:
+class CalculationVisiter(GeometricVisiter):
     def visite_carre(self, carre):
         print(carre.mesure)
     def visite_cercle(self, cercle):
@@ -12,13 +23,13 @@ class CalculationVisiter:
 
 class Carre:
     mesure = "longueur coté"
-    def accept(self, visiteur):
+    def accept(self, visiteur:GeometricVisiter):
         visiteur.visite_carre(self)
 
 
 class Cercle:
     mesure = "rayon"
-    def accept(self, visiteur):
+    def accept(self, visiteur:GeometricVisiter):
         visiteur.visite_cercle(self)
 
 Carre().accept(DisplayVisiter())
